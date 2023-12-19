f4d9a27501d1f799f357cf66e46e57163e4bb91a
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-19 11:07:05 -0500

    Initial Commit

---
+++ Learn SwiftData/Learn_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftDataApp.swift
+//  Learn SwiftData
+//
+//  Created by Danielle Lewis on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
