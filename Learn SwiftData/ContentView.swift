8409ec14e6ecde1f02c784256e2047f37223211c
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-20 10:55:10 -0500

    - create list to show added data items

--- Learn SwiftData/ContentView.swift
+++ Learn SwiftData/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var context
+    
+    // Retrieves all of one data type from the db
+    @Query private var items: [DataItem]
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(items) { item in
+                HStack {
+                    Text(item.creationdate.description)
+                    Spacer()
+                    Button("Update") {
+                        // Updates an item in the db
+                        item.creationdate = Date()
+                    }
+                }
+                .swipeActions {
+                    // Deletes an item from the db
+                    Button("Delete") {
+                        context.delete(item)
+                    }
+                }
+            }
+            
+            Button("Add") {
+                //Create a new data item
+                let data = DataItem()
+                // Add it to the db
+                context.insert(data)
+            }
         }
-        .padding()
     }
 }
 
